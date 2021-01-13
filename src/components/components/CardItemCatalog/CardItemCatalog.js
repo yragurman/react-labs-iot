410c63aaf517e9675a471c0f16b9ee0f9c50036a
Yura <yura.gurman.01@gmail.com>
2021-01-13 04:04:50 +0200

    ss

---
+++ src/components/components/CardItemCatalog/CardItemCatalog.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import {
+  CardWrapper,
+  ButtonWrapper,
+  Select,
+  AddToCart,
+  PriceRateWrapper,
+} from "./CardItemCatalog.styled";
+
+const CardItemCatalog = ({ films }) => {
+  return (
+    <CardWrapper>
+      <ul>
+        {films.map((item) => (
+          <li key={item.id}>
+            <img src={item.image} alt="Film_image"></img>
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+            <PriceRateWrapper>
+              <p>{item.price} grn</p>
+              <p>{item.rate}</p>
+            </PriceRateWrapper>
+            <ButtonWrapper>
+              <Select>Select</Select>
+              <AddToCart>Add to cart</AddToCart>
+            </ButtonWrapper>
+          </li>
+        ))}
+      </ul>
+    </CardWrapper>
+  );
+};
+
+export default CardItemCatalog;
--- src/components/films.js
+++ src/components/films.js
@@ -1,14 +1,70 @@
 import Image from "../image/Films_Image.jpg";
 
 const Films = [
-  { id: "1", image: Image, title: "Film1", description: "VeryNiceFilm1" },
-  { id: "2", image: Image, title: "Film2", description: "VeryNiceFilm2" },
-  { id: "3", image: Image, title: "Film3", description: "VeryNiceFilm3" },
-  { id: "4", image: Image, title: "Film4", description: "VeryNiceFilm4" },
-  { id: "5", image: Image, title: "Film5", description: "VeryNiceFilm5" },
-  { id: "6", image: Image, title: "Film6", description: "VeryNiceFilm6" },
-  { id: "7", image: Image, title: "Film7", description: "VeryNiceFilm7" },
-  { id: "8", image: Image, title: "Film8", description: "VeryNiceFilm8" },
+  {
+    id: "1",
+    image: Image,
+    title: "Film1",
+    description: "VeryNiceFilm1",
+    price: "1337",
+    rate: "8",
+  },
+  {
+    id: "2",
+    image: Image,
+    title: "Film2",
+    description: "VeryNiceFilm2",
+    price: "228",
+    rate: "10",
+  },
+  {
+    id: "3",
+    image: Image,
+    title: "Film3",
+    description: "VeryNiceFilm3",
+    price: "128",
+    rate: "7",
+  },
+  {
+    id: "4",
+    image: Image,
+    title: "Film4",
+    description: "VeryNiceFilm4",
+    price: "399",
+    rate: "5",
+  },
+  {
+    id: "5",
+    image: Image,
+    title: "Film5",
+    description: "VeryNiceFilm5",
+    price: "200",
+    rate: "4",
+  },
+  {
+    id: "6",
+    image: Image,
+    title: "Film6",
+    description: "VeryNiceFilm6",
+    price: "400",
+    rate: "10",
+  },
+  {
+    id: "7",
+    image: Image,
+    title: "Film7",
+    description: "VeryNiceFilm7",
+    price: "300",
+    rate: "9",
+  },
+  {
+    id: "8",
+    image: Image,
+    title: "Film8",
+    description: "VeryNiceFilm8",
+    price: "200",
+    rate: "6",
+  },
 ];
 
 export default Films;
